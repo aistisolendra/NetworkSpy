b03d2784fbeabcd9050e284edaa292805f7369a4
Aistis Olendra <aistis3211@gmail.com>
2021-03-19 04:45:25 +0200

    Add view update class

---
+++ NetworkSpy/Commands/UpdateViewCommand.cs
@@ -0,0 +1,34 @@
+﻿using NetworkSpy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NetworkSpy.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel mainViewModel;
+        public UpdateViewCommand(MainViewModel mainViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Home")
+                mainViewModel.SelectedViewModel = new HomeViewModel();
+            else if (parameter.ToString() == "Connections")
+                mainViewModel.SelectedViewModel = new ConnectionsViewModel();
+        }
+    }
+}
