b353cf688d724681bf3c1c90a3690c520c85b879
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 01:29:17 +0200

    Add "Interfaces" model

---
+++ NetworkSpy/Models/InterfacesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NetworkSpy.Models
+{
+    public class InterfacesModel
+    {
+        public string InterfaceType { get; set; }
+        public string OpStatus { get; set; }
+        public string DNSEnabled { get; set; }
+        public string IPVersion { get; set; }
+    }
+}
