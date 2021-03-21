4ff916972127e663e5a71a4033f7f544dd5e3206
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 01:29:47 +0200

    Update view models

--- NetworkSpy/ViewModels/InterfacesViewModel.cs
+++ NetworkSpy/ViewModels/InterfacesViewModel.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NetworkSpy.Models;
+using Caliburn.Micro;
 
 namespace NetworkSpy.ViewModels
 {
     public class InterfacesViewModel : BaseViewModel
     {
+        public BindableCollection<InterfacesModel> Interfaces { get; set; }
+        public InterfacesViewModel()
+        {
+            Interfaces = new BindableCollection<InterfacesModel>()
+            {
+                new InterfacesModel()
+                {
+                    DNSEnabled = "test",
+                    InterfaceType = "test",
+                    IPVersion = "test",
+                    OpStatus = "test"
+                }
+            };
+        }
     }
 }
