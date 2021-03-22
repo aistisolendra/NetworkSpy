4feffd3d3636f3d69edf02263aa39b42d6501599
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 04:29:12 +0200

    Update interface model

--- NetworkSpy/Models/InterfacesModel.cs
+++ NetworkSpy/Models/InterfacesModel.cs
@@ -2,6 +2,8 @@
 {
     public class InterfacesModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
         public string InterfaceType { get; set; }
         public string OpStatus { get; set; }
         public string DNSEnabled { get; set; }
