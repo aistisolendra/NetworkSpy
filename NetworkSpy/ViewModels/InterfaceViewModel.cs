3cdfa93998da06959fc1520d7829e14cbaf2fcb6
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 04:30:18 +0200

    Implement network interface display

--- NetworkSpy/ViewModels/InterfaceViewModel.cs
+++ NetworkSpy/ViewModels/InterfaceViewModel.cs
@@ -1,5 +1,8 @@
-﻿using NetworkSpy.Models;
+﻿using System.Linq;
+using System.Net.NetworkInformation;
+using NetworkSpy.Models;
 using Caliburn.Micro;
+using NetworkSpy.HelperClasses;
 
 namespace NetworkSpy.ViewModels
 {
@@ -8,16 +11,68 @@ namespace NetworkSpy.ViewModels
         public BindableCollection<InterfacesModel> Interfaces { get; set; }
         public InterfaceViewModel()
         {
-            Interfaces = new BindableCollection<InterfacesModel>()
+            Interfaces = new BindableCollection<InterfacesModel>();
+            GetInterfaces();
+        }
+
+        public void GetInterfaces()
+        {
+            Interfaces.Clear();
+
+            var interfaces = NetworkInterfaces.GetNetworkInterfaces();
+
+            foreach (var intf in interfaces)
             {
-                new InterfacesModel()
+                Interfaces.Add(new InterfacesModel()
                 {
-                    DNSEnabled = "test",
-                    InterfaceType = "test",
-                    IPVersion = "test",
-                    OpStatus = "test"
-                }
-            };
+                    Name = intf.Name,
+                    Description = intf.Description,
+                    InterfaceType = intf.NetworkInterfaceType.ToString(),
+                    IPVersion = GetIPVersion(intf),
+                    DNSEnabled = GetDNSEnabled(intf),
+                    OpStatus = intf.OperationalStatus.ToString()
+                });
+            }
+
+            UpdateInterfaceInformation();
+        }
+
+        private void UpdateInterfaceInformation()
+        {
+            int intfCount = Interfaces.Count();
+            InterfaceCountText = intfCount > 0 ? $"{intfCount} interfaces found" : "No interfaces found";
+        }
+
+        private string GetIPVersion(NetworkInterface intf)
+        {
+            string returnString = "";
+
+            if (NetworkHelper.SupportsIPv4(intf))
+                returnString += "IPv4";
+
+            if (NetworkHelper.SupportsIPv6(intf))
+                if (returnString.Length > 0)
+                    returnString += " IPv6";
+                else
+                    returnString += "IPv6";
+
+            return returnString;
+        }
+
+        private string GetDNSEnabled(NetworkInterface intf)
+        {
+            return intf.GetIPProperties().IsDnsEnabled ? "Enabled" : "Disabled";
+        }
+
+        private string _interfaceCountText;
+        public string InterfaceCountText
+        {
+            get => _interfaceCountText;
+            set
+            {
+                _interfaceCountText = value;
+                NotifyOfPropertyChange(() => InterfaceCountText);
+            }
         }
     }
 }
