3b83a5f17e050fd6067f04aee770eaf139a0388d
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 04:28:29 +0200

    Add NetworkHelper class

---
+++ NetworkSpy/HelperClasses/NetworkHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkSpy.HelperClasses
+{
+    public static class NetworkHelper
+    {
+        public static bool SupportsIPv4(NetworkInterface adapter)
+        {
+            return adapter.Supports(NetworkInterfaceComponent.IPv4);
+        }
+
+        public static bool SupportsIPv6(NetworkInterface adapter)
+        {
+            return adapter.Supports(NetworkInterfaceComponent.IPv6);
+        }
+    }
+}
