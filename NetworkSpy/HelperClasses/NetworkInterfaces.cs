edf3f2f49a3adb726356e3cd75084b38a096c44f
Aistis Olendra <aistis3211@gmail.com>
2021-03-22 04:28:58 +0200

    Add NetworkInterfaces helper class

---
+++ NetworkSpy/HelperClasses/NetworkInterfaces.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace NetworkSpy.HelperClasses
+{
+    public static class NetworkInterfaces
+    {
+        public static List<NetworkInterface> GetNetworkInterfaces()
+        {
+            return NetworkInterface.GetAllNetworkInterfaces().ToList();
+        }
+
+    }
+}
