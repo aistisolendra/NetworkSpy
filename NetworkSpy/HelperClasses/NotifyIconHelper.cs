f87d402b3044461c79c952177451d01226ccb5b1
Aistis Olendra <aistis3211@gmail.com>
2021-03-24 03:07:03 +0200

    Create system tray icon with basic functionality

---
+++ NetworkSpy/HelperClasses/NotifyIconHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using NetworkSpy.Properties;
+
+namespace NetworkSpy.HelperClasses
+{
+    public static class NotifyIconHelper
+    {
+        public static NotifyIcon CreateNotifyIcon()
+        {
+            return new()
+            {
+                Icon = Resources.MainPage_logo,
+                Text = Resources.ProjectName,
+                Visible = true
+            };
+        }
+    }
+}
--- NetworkSpy/ViewModels/ShellViewModel.cs
+++ NetworkSpy/ViewModels/ShellViewModel.cs
@@ -1,14 +1,24 @@
-﻿using Caliburn.Micro;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Forms;
+using Caliburn.Micro;
+using NetworkSpy.HelperClasses;
 
 namespace NetworkSpy.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        private readonly NotifyIcon _notifyIcon;
         private readonly HomeViewModel _homeViewModel = new();
         private readonly InterfaceViewModel _interfacesViewModel = new();
 
         public ShellViewModel()
         {
+            _notifyIcon = NotifyIconHelper.CreateNotifyIcon();
+
+            _notifyIcon.DoubleClick += ShowWindow;
+
             ActivateItem(_homeViewModel);
         }
 
@@ -20,6 +30,30 @@ namespace NetworkSpy.ViewModels
         public void LoadInterfacePage()
         {
             ActivateItem(_interfacesViewModel);
+        }
+
+        private void ShowWindow(object sender, EventArgs e)
+        {
+            MainWindowVisibility = Visibility.Visible;
+        }
+
+        public void OnClose(CancelEventArgs e)
+        {
+            MainWindowVisibility = Visibility.Hidden;
+            e.Cancel = true;
+        }
+
+        private Visibility _mainWindowVisibility;
+
+        [DefaultValue(true)]
+        public Visibility MainWindowVisibility
+        {
+            get => _mainWindowVisibility;
+            set
+            {
+                _mainWindowVisibility = value;
+                NotifyOfPropertyChange(() => MainWindowVisibility);
+            }
         }
     }
 }
