9e9ea41d3cc3d9af950cd4342677969b08ff54d1
Aistis Olendra <aistis3211@gmail.com>
2021-03-26 00:59:06 +0200

    Make NotifyIconService class not static Rename NotifyIconHelper to NotifyIconService

---
+++ NetworkSpy/Services/NotifyIconService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NetworkSpy.Properties;
+
+namespace NetworkSpy.Services
+{
+    public class NotifyIconService
+    {
+        public NotifyIcon NotifyIconInstance { get; set; }
+        public NotifyIconService()
+        {
+            NotifyIconInstance = new NotifyIcon
+            {
+                Icon = Resources.MainPage_logo,
+                Text = Resources.ProjectName,
+                Visible = true
+            };
+        }
+
+        public void AddMenuItem(string name, Image image = null, EventHandler eventHandler = null)
+        {
+            NotifyIconInstance.ContextMenuStrip ??= new ContextMenuStrip() {ShowCheckMargin = true};
+
+            NotifyIconInstance.ContextMenuStrip.Items.Add(name, image, eventHandler);
+        }
+    }
+}
