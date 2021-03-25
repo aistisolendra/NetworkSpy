d09b8f681dc3669a8b3190b6939d437c1d347c98
Aistis Olendra <aistis3211@gmail.com>
2021-03-25 22:34:31 +0200

    Create device model

---
+++ NetworkSpy/Models/DeviceModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace NetworkSpy.Models
+{
+    public class DeviceModel
+    {
+        public string ComputerName { get; }
+        public string MACAddress { get; }
+        public string AssignedIP { get; }
+
+        public DeviceModel(string ipAddress)
+        {
+            AssignedIP = ipAddress;
+            ComputerName = Dns.GetHostEntry(ipAddress).HostName;
+            MACAddress = Dns.GetHostEntry(ipAddress).AddressList[0].ToString();
+        }
+    }
+}
