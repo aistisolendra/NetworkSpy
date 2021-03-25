07a6be3dcaf2ecb5d6dadb63d0a4b35ebed5def3
Aistis Olendra <aistis3211@gmail.com>
2021-03-26 01:00:48 +0200

    Implement device finder logic

--- NetworkSpy/ViewModels/DevicesViewModel.cs
+++ NetworkSpy/ViewModels/DevicesViewModel.cs
@@ -1,8 +1,26 @@
 ﻿using Caliburn.Micro;
+using NetworkSpy.Models;
+using NetworkSpy.Services;
 
 namespace NetworkSpy.ViewModels
 {
     public class DevicesViewModel : Screen
     {
+        public BindableCollection<DeviceModel> Devices { get; set; }
+        private readonly DevicesFinder _devicesFinder;
+        public DevicesViewModel()
+        {
+            _devicesFinder = new DevicesFinder(new[] { "192.168.0.", "192.168.1." });
+            Devices = new BindableCollection<DeviceModel>();
+
+            GetAllDevices();
+        }
+
+        public void GetAllDevices()
+        {
+            Devices.Clear();
+            Devices = _devicesFinder.FindDevices();
+        }
     }
+
 }
