a14b1c6131939d637273ac26debf86e9956a0243
Aistis Olendra <aistis3211@gmail.com>
2021-03-26 00:56:17 +0200

    Refactored and renamed NetworkInterfacesHelper to InterfacesFinder Moved interface creation logic from InterfacesFinder to InterfacesModel

--- NetworkSpy/Models/InterfacesModel.cs
+++ NetworkSpy/Models/InterfacesModel.cs
@@ -1,4 +1,6 @@
-﻿namespace NetworkSpy.Models
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkSpy.Models
 {
     public class InterfacesModel
     {
@@ -8,5 +10,17 @@
         public string OpStatus { get; set; }
         public string DNSEnabled { get; set; }
         public string IPVersion { get; set; }
+
+        public InterfacesModel(NetworkInterface @interface, string ipVersion)
+        {
+            Name = @interface.Name;
+            Description = @interface.Description;
+            Name = @interface.Name;
+            Description = @interface.Description;
+            InterfaceType = @interface.NetworkInterfaceType.ToString();
+            IPVersion = ipVersion;
+            DNSEnabled = @interface.GetIPProperties().IsDnsEnabled.ToString();
+            OpStatus = @interface.OperationalStatus.ToString();
+        }
     }
 }
---
+++ NetworkSpy/Services/InterfacesFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using Caliburn.Micro;
+using NetworkSpy.Models;
+
+namespace NetworkSpy.Services
+{
+    public class InterfacesFinder
+    {
+        private readonly List<NetworkInterface> _allInterfaces;
+        public InterfacesFinder()
+        {
+            _allInterfaces = NetworkInterface.GetAllNetworkInterfaces().ToList();
+        }
+
+        public BindableCollection<InterfacesModel> FindInterfaces()
+        {
+            BindableCollection<InterfacesModel> interfacesModels = new();
+
+            foreach (var @interface in _allInterfaces)
+            {
+                interfacesModels.Add(new InterfacesModel(@interface, GetIPVersions(@interface)));
+            }
+
+            return interfacesModels;
+        }
+
+        public string GetIPVersions(NetworkInterface @interface)
+        {
+            var ipVersions = new List<string>();
+
+            if (@interface.Supports(NetworkInterfaceComponent.IPv4))
+                ipVersions.Add(NetworkInterfaceComponent.IPv4.ToString());
+
+            if (@interface.Supports(NetworkInterfaceComponent.IPv6))
+                ipVersions.Add(NetworkInterfaceComponent.IPv6.ToString());
+
+            return string.Join(" ", ipVersions);
+        }
+    }
+}
